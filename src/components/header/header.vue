44dfb87224fa6977d36a2a914c38fbd6ad93030c
byCHENJIAJIE <bychenjiajie@163.com>
2017-09-29 23:45:39 +0800

    start

---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="header">
+    我是header
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style rel="stylesheet/css">
+
+</style>
+
